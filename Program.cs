074609de6e07092961a44dc93f82c0084b07b77e
singhet <singhet@SCSWL4072>
2024-01-15 12:51:09 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double C, F;
+        Console.WriteLine("Wpisz stopnie Celsjusza");
+        C = double.Parse(Console.ReadLine());
+        // program wyliczy stopnie Farenheita na podstawie podanych stopni Celsjusza
+        //Program will calculate Fahrenheit degrees based on the provided Celsius degree
+        F = 32 + (9d / 5 * C);
+        Console.Write("Jest to: {0} stopni Fahrenheita.", F);
+        Console.ReadKey();
+    }
+}
